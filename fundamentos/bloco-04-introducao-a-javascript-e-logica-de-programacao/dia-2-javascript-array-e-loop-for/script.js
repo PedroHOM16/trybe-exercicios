66af7a240cd493c64b19bbb097010810d71c9414
Pedro Henrique <pedro.admoura@gmail.com>
2021-11-30 15:35:34 -0300

    Exercicio 6 feito

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -15,4 +15,15 @@ for (let i = 0; i < numbers.length; i += 1) {
         maiorNumero = numbers[i]
     } 
 }
-console.log('Exercício 4: O maior número da lista é' , maiorNumero)
+console.log('Exercício 5: O maior número da lista é' , maiorNumero)
+let quantidadeImpar = 0
+for (i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 2 !== 0) {
+        quantidadeImpar += 1
+    }    
+}
+if (quantidadeImpar !== 0) {
+    console.log("Exercício 6: Quantidade de números ímpar:", quantidadeImpar)
+} else {
+    console.log("Exercício 6: Não há nenhum número ímpar!")
+}
