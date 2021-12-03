55a1a45ec85904ce7199a1cb9db3db56a57932e8
Pedro Henrique <pedro.admoura@gmail.com>
2021-12-03 18:03:34 -0300

    Criação diretorio bloco 5

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -0,0 +1,9 @@
+let ast = 5;
+let result = '';
+for (i = 0; i < ast; i += 1){
+    result += '*';
+    for (index = 0; index < result.length; index +=1){
+        console.log(result)
+    }
+}
+//console.log(result)
